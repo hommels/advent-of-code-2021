619acf6051515aa7e7a2eb37ec110c7d6b4bf484
Simon <hommels@protonmail.com>
2021-12-04 19:57:59 +0100

    Day 1: renamed variable

--- day1/main.py
+++ day1/main.py
@@ -1,23 +1,22 @@
 import sys
 
-def get_increases(nums):
+def count_increases(nums):
     total = 0
     for i in range(1, len(nums)):
         if nums[i] > nums[i-1]:
             total += 1
 
     return total
-    
 
 def main():
     with open("input", 'r') as f:
-        measurements = [int(x) for x in f.readlines()]
+        data = [int(x) for x in f.readlines()]
 
-    anwser1 = get_increases(measurements) 
+    part1 = count_increases(data) 
     print(f"Part 1: {anwser1}")
 
-    windows = [sum(measurements[i:i + 3]) for i in range(len(measurements) - 2)]
-    answer2 = get_increases(windows)
+    windows = [sum(data[i:i + 3]) for i in range(len(data) - 2)]
+    part1 = count_increases(windows)
     print(f"Part 2: {answer2}")
 
 if __name__ == '__main__':
